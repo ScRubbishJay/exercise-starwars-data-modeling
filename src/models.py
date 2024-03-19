60ca3ea60ffe1d5003799d6afe658a7e1b3eb845
Jvonte Walker <107204895+ScRubbishJay@users.noreply.github.com>
2024-03-19 21:37:46 +0000

    star wars

--- src/models.py
+++ src/models.py
@@ -7,25 +7,39 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class People(Base):
+    __tablename__ = 'people'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    appearances = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    appearances = Column(String(250), nullable=False)
 
-    def to_dict(self):
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    appearances = Column(String(250), nullable=False)
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+
+class User(Base):
+   __tablename__ = 'user'
+   name = Column(String(250), nullable=False)
+   id= Column()
+   email = Column(String(250), nullable=False)
+   Favorites = Column(String(250), nullable=False)
+
+
+
+def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
